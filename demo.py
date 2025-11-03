cc7b79563670d192d148b74ab9030b8022932817
bijoydasgupta <bijoydasgupta595@gmail.com>
2025-11-04 00:06:42 +0600

    first commit

---
+++ demo.py
@@ -0,0 +1,19 @@
+# First basic codes
+print("\nhello")
+print("World")
+
+print(2 > 5)
+print(5 > 2)
+print(5+2)
+
+print("*" * 5)
+# Second basic codes
+print("I am ", 25, "years old")
+if 5 > 2:
+    print("Five is greater than two!")
+"""
+This is a comment
+written in
+more than just one line
+"""
+print("Hello, World!")
---
+++ variables.py
@@ -0,0 +1,15 @@
+# Varibles
+students_count = 1000
+rating = 4.99
+isPublished = True
+is_active = False
+
+print("")
+print(students_count)
+print(rating)
+print(isPublished)
+print(is_active)
+
+course_name = "Python for Beginners"
+print(course_name)
+print("")
