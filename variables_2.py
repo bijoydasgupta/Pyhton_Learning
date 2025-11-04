bde89564a8646b4c73e61072155888ba5a82d2d2
bijoydasgupta <bijoydasgupta595@gmail.com>
2025-11-04 14:58:16 +0600

    assigning values

--- variables_2.py
+++ variables_2.py
@@ -1,6 +1,7 @@
 x = str(3)
 y = int(3)
 z = float(3)
+print()  # New Line
 print(x)
 print(y)
 print(z)
@@ -8,4 +9,24 @@ print(z)
 f = 5
 b = "John"
 print(type(f))
-print(type(b))
+print(type(b))  # Getting the data type of the variables
+
+print()  # New Line
+x, y, z = "Orange", "Banana", "Cherry"  # Many Values to Multiple Variables
+print(x)
+print(y)
+print(z)
+print()  # New Line
+
+x = y = z = "Orange"  # One Value to Multiple Variables
+print(x)
+print(y)
+print(z)
+print()  # New Line
+
+# Unpack a Collection
+fruits = ["apple", "banana", "cherry"]
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
