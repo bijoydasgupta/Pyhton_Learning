03366885b553da76d5b3d2599238ce8fe5cf8175
bijoydasgupta <bijoydasgupta595@gmail.com>
2025-11-05 01:32:27 +0600

    comma and plus

---
+++ variables_3.py
@@ -0,0 +1,21 @@
+print()  # New Line
+x = "Python"
+y = "is"
+z = "cool"
+print(x, y, z)
+
+print()  # New Line
+p = "I am "
+q = "enjoying "
+r = "it"
+print(p + q + r)
+
+print()  # New Line
+u = 5
+v = "Bijoy"
+print(u, v)
+
+print()  # New Line
+u = 5
+v = "Bijoy"
+print(u + v)  # Python will give you an error
