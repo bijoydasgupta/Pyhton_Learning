bc3132af0469fbb7a1ff22834f7127a826232c4a
bijoydasgupta <bijoydasgupta595@gmail.com>
2025-11-05 16:12:55 +0600

    this is string code

---
+++ strings.py
@@ -0,0 +1,25 @@
+print()
+print("Hey, Man!")  # Double Quotes
+print('Hope you are great!')  # Single Quotes
+print()  # New Line
+
+print("I am 'Bijoy'")  # Quotes Inside Quotes
+print('I am Learning "Python"')  # Quotes Inside Quotes
+print()  # New Line
+
+a = "Python is awesome"  # Assigning String to a Variable
+print(a)
+print()  # New Line
+
+b = """Hey, Man! Hope you are great!,
+I am 'Bijoy',
+I am Learning 'Python',
+I will try my best! """
+print(b)
+
+print()  # New Line
+c = "Hello World"
+print("6-Indexed value of 'Hello World':", c[6])
+
+print()  # New Line
+print("Length of 'Hello World':", len(c))
