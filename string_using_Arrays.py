3ac538123b8a6e7f44b1841cae1098fb74d16078
bijoydasgupta <bijoydasgupta595@gmail.com>
2025-11-07 00:27:40 +0600

    string using array

---
+++ string_using_Arrays.py
@@ -0,0 +1,13 @@
+print()  # New Line
+c = "Hello World"
+print("The String:", c)
+print()  # New Line
+print("c[0]:", c[0])
+print("c[-1]:", c[-1])
+print("c[-2]:", c[-2])
+
+print()  # New Line
+print("Slicing String (c[0:3]):", c[0:3])
+print("c[2:] =", c[2:])
+print("c[:3] =", c[:3])
+print("c[:] =", c[:])
