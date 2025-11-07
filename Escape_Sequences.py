56dcb846c3bfdea4d0ade9e80622386a1cdfd9d4
bijoydasgupta <bijoydasgupta595@gmail.com>
2025-11-07 22:00:44 +0600

    esc

---
+++ Escape_Sequences.py
@@ -0,0 +1,19 @@
+print()  # New Line
+course = "Python \" Progrmming"
+print(course)
+
+print()  # New Line
+course = 'Python " Progrmming'
+print(course)
+
+print()  # New Line
+c = "Hello \' World"
+print(c)
+
+print()  # New Line
+d = "Hello \\ Bijoy"
+print(d)
+
+print()  # New Line
+course = "Python \nProgrmming"
+print(course)
