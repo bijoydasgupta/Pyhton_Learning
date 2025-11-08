2b7965469c23e1e06274bba7f47916c8d07f00a1
bijoydasgupta <bijoydasgupta595@gmail.com>
2025-11-09 00:02:19 +0600

    string

---
+++ Formatted_Strings_Methods.py
@@ -0,0 +1,17 @@
+print()  # New Line
+a = "Hello"
+b = "World"
+c = a + " " + b
+print(c)
+
+print()  # New Line
+first = "Bijoy"
+last = "Das"
+full = f"{first}{last}"
+fullName = f"{first} {last}"
+print(full)
+print(fullName)
+
+print()  # New Line
+result = f"{len(first)} {2+2}"
+print(result)
