aecbf3ae7aecc511ceb7f7e799652383ae7299bc
bijoydasgupta <bijoydasgupta595@gmail.com>
2025-11-13 01:35:08 +0600

    op & st

---
+++ Operators_&_Statements.py
@@ -0,0 +1,17 @@
+print("\nComparison Operators:")
+print("10 > 3:", 10 > 3)
+print("10 >= 3:", 10 >= 3)
+print("10 < 20:", 10 < 20)
+print("10 <= 3:", 10 <= 20)
+print("10 == 10:", 10 == 10)
+print("10 == '10':", 10 == "10")
+print("10 != '10':", 10 != "10")
+print("'bg' < 'dog':", 'bg' < 'dog')
+print("'bg' == 'BG':", 'bg' == 'BG')
+
+print("\nConditional Statements:")
+temperture = 35
+if temperture > 30:
+    print("It's hot")
+    print("Drink water")
+print("Done")
