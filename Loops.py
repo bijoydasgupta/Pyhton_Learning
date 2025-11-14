f14797615c3e58d97566f1bbe6caea40ab3cc6b6
bijoydasgupta <bijoydasgupta595@gmail.com>
2025-11-15 01:36:45 +0600

    for loop

---
+++ Loops.py
@@ -0,0 +1,12 @@
+print("\nFor Loops:")
+for number in range(3):
+    print("attempt")
+for number in range(3):
+    print("attempt", number + 1, (number + 1) * ".")
+for number in range(1, 5):
+    print("attempt", number, number * ".")
+for number in range(1, 10, 3):
+    print("attempt", number, number * ".")
+
+
+print("\nFor...else:")
